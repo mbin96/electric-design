39bb81c29255b8ef1589956bb3d796fddab52fd3
xeca <mbin96@gmail.com>
2019-03-22 00:19:27 +0900

    excercize

--- 3rd/ex6.c
+++ 3rd/ex6.c
@@ -1,1 +1,11 @@
-ttddfjkfdkljasdfjklasdfjkadfasdf
+#include <stdio.h>
+
+int main(int argc,char*argv[]){
+    int fi;
+
+    printf("main function get %d argv\n",argc);
+    for (fi = 0; fi < argc; fi++){
+        printf("argv[%d] : %s\n", fi, argv[fi]);
+    }
+    scanf("%d");
+}
