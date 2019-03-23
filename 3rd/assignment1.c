79bebf3d923866fc4d1a46a93576d7973d89126b
xecachan <mbin96@gmail.com>
2019-03-23 17:05:46 +0900

    주석 달기 끝

--- 3rd/assignment1.c
+++ 3rd/assignment1.c
@@ -1,18 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-//int countByFor();
-//int countByWhile();
+
+int countByFor();           //for문으로 계산하는 함수의 선언     
+int countByWhile();         //while문으로 계산하는 함수의 선언
+
 int main(){
     int countSpace = 0;     //스페이스의 숫자를 세는 변수
     char str[1001];         //마지막은 nul 들어가야해서 1001칸 배열 필요
     scanf("%[^\n]s",str);   //str에 키보드 입력 받음
 
-    for(int i = 0; str[i] != 0; i++){   //하나씩 읽으면서 스페이스가 있는지 검사. 
+    countSpace = countByFor(str);   //for문을 이용해서 공백 세기.
+    printf("counted by for      : %d\n", countSpace);   //for문으로 센 스페이스 출력
+
+    countSpace = 0;         //초기화
+    countSpace = countByWhile(str); //while문을 이용한 공백세기
+    printf("counted by while    : %d\n", countSpace);   //while문으로 센 스페이스 출력
+
+}
+
+int countByFor(char * str){
+    int count = 0;          //카운트 변수 선언
+    for(int i = 0; str[i] != 0; i++){   //str[i]이 NUL(ascii로 0)이 나올때까지 하나씩 읽으면서 스페이스가 있는지 검사. 
         
-        if (str[i]==32){    //ascii코드에서 32(space)를 발견하면 countSpace변수 숫자 +1
-            countSpace++;   
+        if (str[i]==32){    //ascii코드에서 32(space)를 발견
+            count++;        //space발견하면 1 증가
         }
 
     }
-    printf("%d", countSpace);
+    return count;           //카운트를 반환
+}
+ 
+int countByWhile(char * str){
+    int count = 0, i = 0;   //카운트 변수와 i변수를 선언하며 초기화
+    while(str[i] != 0){
+         if (str[i]==32){   //ascii코드에서 32(space)를 발견하면 countSpace변수 숫자 +1
+            count++;        //space발견하면 1 증가
+        }
+        i++;                //i증가
+    }
+    return count;           //카운트를 반환
+    
 }
--- 3rd/assignment2.c
+++ 3rd/assignment2.c
@@ -1,83 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>           //random 함수를 사용하기위해 include
 
 
 
 void statistic(int * arr, int arrSize, float * avr, int * max, int * min){
-    float sum =0;
-    int fi = 0;
+    float sum = 0;          //총합을 구하는 변수 sum선언과 초기화
+    int fi = 0;             //arr를 하나씩 접근하기위한 변수 fi선언과 초기화
 
  //////////////////////////과제/////////////////////////////
-    while(fi < arrSize){ //avg구하기 시작
-        sum += arr[fi];
-        fi++;
+ 
+    for(; fi < arrSize; fi++){  //avg구하기 시작. arrsize보다 작은동안 실행.
+        sum += arr[fi];         //sum변수와 현재 arr값을 더하기
     }
-    *avr = sum / arrSize;  //avg구함
+    *avr = sum / arrSize;       //avg구함
 
      
-    //맥스구하기 시작
-    *min = arr[0];
-    *max = arr[0];
+    //맥스와 민값구하기 시작
+    *min = arr[0];              //min값과 max값의 초기값을 arr의 첫번째 값으로 넣어줌.
+    *max = arr[0];              //첫값을 int_max나 int_min값으로 지정하고 비교하는것보다 더 효율적이다.
     for(fi = 1; fi < arrSize; fi++){
-        if (*max < arr[fi]){
-            *max = arr[fi];  //비교해서 arr [fi]값이 *max값보다 크면 *max값 갱신
+        if (*max < arr[fi]){    //맥스 값 찾기
+            *max = arr[fi];     //현재 저장된값과 비교해서 arr [fi]값이 *max값보다 크면 *max값 갱신
         }
-        if (*min > arr [fi]){
-            *min = arr [fi]; //비교해서 arr [fi]값이 *min값보다 작으면 *min값 갱신
+        if (*min > arr [fi]){   //민 값 찾기
+            *min = arr [fi];    //비교해서 arr [fi]값이 *min값보다 작으면 *min값 갱신
         }
-
     }
-
-    //////////////////////////과제//////////////////////////
+///////////////////////////과제//////////////////////////
 
 }
 
 
 
-int main (int argc, void * argv[]){
-    int n = 0;
-    int *ptr = NULL;
-    float avr = 0;
-    int max = 0, min = 0 , fi;
+int main (int argc, void * argv[]){ //프로그램을 실행할때 argv에 문자열을, argc에 문자열의 개수를 저장 
+    int n = 0;                      //실행하면서 받은 문자열의 숫자를 저장할 변수
+    int *ptr = NULL;                //포인터 선언과 동시에 포인터의 주소를 NULL(0)으로 초기화 함
+    float avr = 0;                  //평균값을 저장할 float변수 선언
+    int max = 0, min = 0 , fi;      //맥스값과 최소값을 저장할 변수와 for에서 루프 카운트를 위한 변수 선언
 
-    srand((unsigned)time(NULL));
+    srand((unsigned)time(NULL));    //랜덤함수 선언
 
     if(argc < 2){
-        printf("CHK cmd Input!!!\n");
-        return -1;
+        printf("CHK cmd Input!!!\n");   //argv가 입력이 되지 않았으면 종료
+        return -1;                      //에러 디버깅용 -1 리턴
     }
 
-    n = atoi(argv[1]);
+    n = atoi(argv[1]);                  //argv에 입력받은 char문자열을 int형으로 변환
 
     if (n<1){
-
-        printf("CHK cmd input!!!!\n");
-
-        return -1;
-
+        printf("CHK cmd input!!!!\n");  //argv가 1이하로 입력되었으면 종료
+        return -1;                      //에러 디버깅용 -1 리턴
     }
 
-    printf("N = %d\n",n);
+    printf("N = %d\n",n);               //입력받았던 숫자 출력
 
 //////////////////////과제//////////////////////////
 
-    ptr = (int *)calloc( n ,sizeof(int));
+    ptr = (int *)calloc( n ,sizeof(int));       //입력받았던 숫자만큼 포인터배열 생성
 
     for (fi=0; fi<n; fi++){
-        ptr[fi] = rand() % 101;
-        printf("ptr[%d] = %d\n",fi ,ptr[fi]);
+        ptr[fi] = rand() % 101;                 //랜덤함수를 통해 0부터 100까지의 숫자를 생성하여 포인터에 하나씩넣기
+        printf("ptr[%d] = %d\n",fi ,ptr[fi]);   //생성된 랜덤값을 출력
     }
     
-    statistic(ptr, n, &avr, &max, &min);
+    statistic(ptr, n, &avr, &max, &min);        //평균값, 최대 최소값 구하는 함수 호출
 
 //////////////////////과제//////////////////////////
 
-    printf("average : %f\n",avr);
-    printf("max : %d\n",max);
-    printf("min : %d\n",min);
-    free(ptr);
-    return 0;
+    printf("average : %f\n",avr);       //평균값 출력
+    printf("max : %d\n",max);           //맥스값 출력
+    printf("min : %d\n",min);           //최소값 출력
+    free(ptr);                          //포인터 메모리 반환
+    return 0;                           
 
    
 
