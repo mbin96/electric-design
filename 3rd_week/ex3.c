8e798284816d1c11ff7bf78a68646efcad47284d
xecachan <mbin96@gmail.com>
2019-03-27 00:38:36 +0900

    주석추가

--- 3rd_week/ex3.c
+++ 3rd_week/ex3.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int a = 10;
-    double b = 7.25;
+    int a = 10;         
+    double b = 7.25;    //정수와 부동소수점 변수 선언
 
     int *pt_a;
-    double *pt_b;
+    double *pt_b;       //정수와 부동소수점 포인터 선언
 
     pt_a = &a;
-    pt_b = &b;
+    pt_b = &b;          //포인터에 각 변수의 주소 입력
     printf("sizeof(pt_a) = %d\n",sizeof(pt_a));
-    printf("sizeof (pt_b) = %d\n",sizeof(pt_b));
+    printf("sizeof (pt_b) = %d\n",sizeof(pt_b));    //각 포인터의 사이즈 출력
 
-    printf("inta - %d\n",a);
-    printf("doublr b = %lf\n\n",b);
-    printf("a;s address = %p\n", pt_a);
-    printf("b's address = %p\n\n",pt_b);
+    printf("int a - %d\n",a);    
+    printf("double b = %lf\n\n",b);                 //각 변수의 값 출력
+    
+    printf("a's address = %p\n", pt_a);
+    printf("b's address = %p\n\n",pt_b);            //각 변수의 주소값 출력
 
-    a= 12;
-    *pt_a = 15;
-
+    a = 12;  
+    *pt_a = 15;         
     b = 8.5;
-    *pt_b = 10.75;
+    *pt_b = 10.75;      //두 변수의 값을 직접 바꾼뒤에 포인터로 역참조하여 바꿔줌
 
-    printf("int a = %d\n",a);
-    printf("double b = %lf\n\n",b);
-
-
+    printf("int a = %d\n",a);                       //각 변수의 값출력
+    printf("double b = %lf\n\n",b);                 //이때 변수의 값은 역참조하여 바꾼값으로 출력된
 
 }
--- 3rd_week/ex4.c
+++ 3rd_week/ex4.c
@@ -3,39 +3,35 @@
 #include <time.h>
 
 int main(){
-    int arr[100];
-    int * ptr_arr;
-    int fi;
+    int arr[100];           //정수 어레이 선언
+    int * ptr_arr;          //포인터 선언
+    int fi;                 //루프 카운터 변수 선언
 
-    srand((unsigned)time(NULL));
+    srand((unsigned)time(NULL));    //랜덤 함수 선언
 
-    ptr_arr = &arr[0];
+    ptr_arr = &arr[0];              //포인터에 어레이의 첫번째 요소 주소 입력
 
-    printf("address od arr[0] is %p\n", &arr[0]);
-    printf("arr is pointing %p\n",arr);
-    printf("ptr_arr is pointing %p\n",ptr_arr);
+    printf("address of arr[0] is %p\n", &arr[0]);       //어레이의 첫요소 주
+    printf("arr is pointing %p\n",arr);                 //어레이 자체가 가리키는(포인터) 값의 출력
+    printf("ptr_arr is pointing %p\n",ptr_arr);         //포인터의 값을 출력
     printf("\n");
-    printf("addressof arr[1] is %p\n", &arr[1]);
-    printf("arr+1 is pointing %p\n", arr+1);
-    printf("ptr_aarr+1 is pointing %p\n", ptr_arr+1);
+    printf("addressof arr[1] is %p\n", &arr[1]);        //어레이의 두번째 요소 주소
+    printf("arr+1 is pointing %p\n", arr+1);            //어레이 자체가 가리키는(포인터) 값 + 1 의 출력
+    printf("ptr_aarr+1 is pointing %p\n", ptr_arr+1);   //포인터의 값 + 1 의 출력
     printf("\n");
 
-    for (fi=0; fi < 100; fi++){
+    for (fi=0; fi < 100; fi++){     //포문을 이용해 랜덤한 0부터 100까지의 정수를 어레이에 넣음
         arr[fi] =rand() % 101;
     }
 
-    printf("arr[0] = %d\n", arr[0]);
-    printf("*arr = %d\n",*arr);
-    printf("*ptr_arr = %d\n",*ptr_arr);
-    printf("\n");
-    printf("arr[20] = %d\n", arr[20]);
-    printf("*(arr+20) = %d\n", *(arr+20));
-    printf("*(ptr_arr+20= %d\n",*(ptr_arr+20));
-    printf("*arr_20 = %d\n",*arr +20);
-    printf("*ptr_arr+20 = %d\n", *ptr_arr + 20);
-
-
-
-    
+    printf("arr[0] = %d\n", arr[0]);                    //어레이의 첫요소 값 출력
+    printf("*arr = %d\n",*arr);                         //어레이를 역참조하여 값 출력
+    printf("*ptr_arr = %d\n",*ptr_arr);                 //포인터를 역참조하여 값출력
+    printf("\n");                   
+    printf("arr[20] = %d\n", arr[20]);                  //어레이의 21번요소 값 출력
+    printf("*(arr+20) = %d\n", *(arr+20));              // 어레이자체의 포인터 + 20 한 값의 역참조값 출력
+    printf("*(ptr_arr+20= %d\n",*(ptr_arr+20));         //포인터 + 20 한 값의 역참조값의 출력
+    printf("*arr_20 = %d\n",*arr +20);                  //어레의 첫요소 값 + 20 값 출력
+    printf("*ptr_arr+20 = %d\n", *ptr_arr + 20);        //포인터의 역참조값 + 20 의 출력
 
 }
