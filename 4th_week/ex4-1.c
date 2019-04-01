30b4530f3ebdd07470adf86c97085dfa98d103cc
xeca <mbin96@gmail.com>
2019-04-02 00:32:09 +0900

    주석 담

--- 4th_week/ex4-1.c
+++ 4th_week/ex4-1.c
@@ -2,28 +2,33 @@
 #include <stdlib.h>
 
 int main(){
-
+    //파일포인터 선언
     FILE *fp;
+    //스트링 선언
     char *str = "each undeclared identifier is reported only once for each function it appears in The terminal process terminated with exit code: ";
-
+    //텍스트 기반 파일 입출력을 위한 버퍼 선언
     char buf[200];
     int fi;
 
+    //파일포인터에 텍스트 기반 쓰기로 파일을 연다. 실패할경우 NULL이 반환되어 종료된다.
     fp = fopen("test.txt", "wt");
     if(fp == NULL){
         printf("fail to open file\n");
         return -1;
     }
 
+    //파일에 char을 쓴다. 
     fputc('t', fp);
     fputc('e', fp);
     fputc('s', fp);
     fputc('t', fp);
     fputc('\n', fp);
-
+    //파일에 string을 쓴다.
     fputs(str, fp);
+    //파일을 닫는다.
     fclose(fp);
 
+    //파일포인터에 파일을 텍스트 기반 읽기로 연다.
     fp = fopen("test.txt","rt");
 
     for(fi = 0; fi < 5; fi++){
--- 4th_week/ex5.c
+++ 4th_week/ex5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//student 구조체 선언 및 별칭 선언
 typedef struct student
 {
     char grade;
@@ -10,24 +11,28 @@ typedef struct student
 }STD;
 
 int main(){
-    FILE *fp;
-    STD st;
+    FILE *fp;   //파일포인터 선언
+    STD st;     //구조체 변수 선언
 
-    fp = fopen("student.txt","wt");
+    //파일포인터에 파일을 연뒤에 열리지 않았을경우 에러메세지 출력
+    fp = fopen("student.txt","wt"); //텍스트 쓰기모드로 열기
     if (fp == NULL){
         printf("fail to open file \n");
     }
 
+    //구조체 변수에 값 입력
     st.grade = 'B';
     st.midTerm = 56;
     st.finalTerm = 72;
     st.attendance = 10;
 
+    //구조체변수의 값 파일포인터를 이용해 파일에 출력
     fprintf(fp, "grade is %d\n", st.grade);
     fprintf(fp, "midterm is %d\n", st.midTerm);
     fprintf(fp, "finalterm is %d\n", st.finalTerm);
     fprintf(fp, "attendance is %d\n", st.attendance);
     
+    //파일을 닫아줌.
     fclose(fp);
 
     return 0;
