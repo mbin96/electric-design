e5643ffba9e97a2bb448af14d93ce27e0b340bc7
xeca <mbin96@gmail.com>
2019-04-02 21:22:11 +0900

    주석달기

--- 4th_week/ex1.c
+++ 4th_week/ex1.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-struct student{
+//student구조체의 정의와 선언
+struct student{         
     char grade;
     int midTerm;
     int finalTerm;
     int attendance;
 };
-
+//메인함수 시작
 int main(){
-    struct student std1;
+    struct student std1;    //함수내에서 구조체 변수 정의
+
     std1.grade = 'a';
     std1.midTerm = 97;
     std1.finalTerm=92;
-    std1.attendance = 10;
+    std1.attendance = 10;   //구조체 변수의 선언
 
+    //구조체 변수의 구성원 출력
     printf("std1's grade is %c\n", std1.grade);
     printf("std1's midTerm is %d\n", std1.midTerm);
     printf("std1's final term id %d\n", std1.finalTerm);
