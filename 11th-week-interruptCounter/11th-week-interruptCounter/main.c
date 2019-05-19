3de16277d8a2956e2b6767ebb4483aea2276f59c
cho <mbin96@gmail.com>
2019-05-19 22:19:12 +0900

    주석 달기 완료

--- 11th-week-interruptCounter/11th-week-interruptCounter/main.c
+++ 11th-week-interruptCounter/11th-week-interruptCounter/main.c
@@ -1,108 +1,103 @@
-/*
- * CFile1.c
- *
- * Created: 2019-05-16 오후 3:36:44
- *  Author: mbin9
- */ 
-/*
- * 11th-week-timeclock.c
- *
- * Created: 2019-05-15 오전 10:05:34
- * Author : hyeon
- */ 
-/////////////////define include, global variable////////////
-#define F_CPU 16000000UL
+
+/////////////////define, include, global variable////////////
+//define frequency and chip
+#define F_CPU 16000000UL        //atmega128A is 16 MHz
+#define __AVR_ATmega128__        //we're using atmega128A
+
+//include
 #include <avr/io.h>
 #include <avr/interrupt.h>
-unsigned timeNum = 0, timeIterruptSet = 0;
-unsigned char FND_SEGNP[10] = {0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x27,0x7F,0x6F};//숫자
-unsigned char FND_SEGWP[10] = {0xBF,0x86,0xDB,0xCF,0xE6,0xED,0xFD,0xA7,0xFF,0xEF};//점있는숫자
-unsigned char FND_SEGPOS[4] = {0x01,0x02,0x04,0x08};//세그먼트 자리
-unsigned int exp10[4] = {1,10,100,1000};
-#define STATE_INIT 0
-#define STATE_TIMER_RUNNING 1
-#define STATE_TIMER_PAUSE 2
-unsigned int state = STATE_INIT;
-//////////////////////
 
-void printSeg(int num, int whatSegment){
-	//자리수 지정
-	PORTA = FND_SEGPOS[whatSegment];
-	PORTE = 0x04;
-	PORTE = 0x00;
-	//세그먼트 숫자출력
-	PORTA = FND_SEGNP[num];
-	PORTE = 0x08;
-	PORTE = 0x00;
-	
+//global variable declaration                                                         //initialize state
+unsigned int num = 0;                                        //variable for time interrupt
+unsigned char FND_SEGNP[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x6F}; //7segment decimal LED print with out point
+unsigned char FND_SEGWP[10] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0xA7, 0xFF, 0xEF}; //7segment decimal LED print with point
+unsigned char FND_SEGPOS[4] = {0x01, 0x02, 0x04, 0x08};                                     //7segment's location
+unsigned int exp10[4] = {1, 10, 100, 1000};                                                 //print 10^x (for divide num variable)
+///////////////////////////////////////////////////////////
+
+
+void printSeg(int num, int segDigit){
+    //by schematic, PORTE must being clear before set segment's digit and led.
+    //because circuit use same port(A) to set segment's digit and led by latch.
+
+    //set segment digit to print
+    PORTA = FND_SEGPOS[segDigit];
+    PORTE = 0x04; //set 1 segment digit latch
+    PORTE = 0x00; //clear port E
+
+    //print num at segment
+    if(segDigit == 1) PORTA = FND_SEGWP[num];    //print with decimal point
+    else PORTA = FND_SEGNP[num];                 //print with out decimal point
+    PORTE = 0x08; //set 1 segment led latch
+    PORTE = 0x00; //clear port E
 }
 
 void initPort(){
-	DDRA = 0xFF;
-	DDRF = 0xF0;
-	DDRG = 0x00;
-	DDRE = 0x0C;
-	PORTE = 0x04;
-	PORTA = 0x0F;
+    //initialize port A and E
+    DDRA = 0xFF; //set portA(7:0) to output
+    DDRE = 0x0C; //set portE(3:2) to output
+    PORTE = 0x04;//init port E
+    PORTA = 0x0F;//init port A
 }
 
 void initInterrupt(){
-	
-	//external interrupt
-	
-	//int4 버튼 falling edge활성화
-	EICRB  &= ~(1<<ISC40);
-	EICRB  |= 1<<ISC41;
-	//int4 입력 활성화
-	EIMSK |= 1<<INT4;    
-	
-	//int5 버튼 falling edge활성화
-	EICRB  &= ~(1<<ISC50);	//0으로 설정
-	EICRB  |= 1<<ISC51;		//1로 설정
-	//int5 입력 활성화
-	EIMSK |= 1<<INT5;
-	
+    
+    //external interrupt
+    //external interrupt int4 enable
+    //active int4 when falling edge
+    EICRB &= ~(1 << ISC40);
+    EICRB |= 1 << ISC41;     //same to EICRB = 0dxxxxxx10
+    //int4 입력 활성화
+    EIMSK |= 1 << INT4;
+
+    //external interrupt int5 enable
+    //active int5 when falling edge
+    //same to EICRB = 0dxxxx10xx
+    EICRB &= ~(1 << ISC50); //0으로 설정
+    EICRB |= 1 << ISC51;    //1로 설정
+    //int5 입력 활성화
+    EIMSK |= 1 << INT5;
 }
+
 ISR(INT4_vect)
 {
-	if(timeNum == 9999){
-		timeNum = 0;
-	}else{
-		timeNum++; 
-	}
-	
-	
-	
+    //if int4 BT press, decrease num
+    //however, if num==9999 and BT pressed, set num to 0 
+    if(num == 9999){
+        num = 0;
+    }else{
+        num++; 
+    }
 }
+
 ISR(INT5_vect)
 {
-	if(timeNum == 0){
-		timeNum = 9999;
-		}else{
-		timeNum--;
-	}
-	
+    //if int5 BT press, decrease num
+    //however, if num==0 and BT pressed, set num to 9999 
+    if(num == 0){
+        num = 9999;
+    }else{
+        num--;
+    }
 }
 
 
 
 int main(void)
 {
-	
-	initPort();
-	initInterrupt();
+    //initialize port and interrupt
+    initPort();
+    initInterrupt();
 
-	sei();      //Global Interrupt Enable
-	
-	while (1){
-		
-		//segment print
-		for(int i = 0; i < 4; i++){
-			printSeg((timeNum/exp10[i])%10,3-i);
-		}
-		//초기화
-		
-		
-	}
-	
+    //Global Interrupt Enable
+    sei();      
+    //print 7segment by global variable num
+    //num variable is increase by interrupt
+    while (1){
+        //segment print
+        for(int i = 0; i < 4; i++){
+            printSeg((num/exp10[i])%10,3-i);
+        }
+    }
 }
--- 11th-week-timeclock/11th-week-timeclock/main.c
+++ 11th-week-timeclock/11th-week-timeclock/main.c
@@ -1,130 +1,125 @@
-/*
- * 11th-week-timeclock.c
- *
- * Created: 2019-05-15 오전 10:05:34
- * Author : hyeon
- */ 
+
 /////////////////define, include, global variable////////////
 //define frequency and chip
 #define F_CPU 16000000UL        //atmega128A is 16 MHz
 #define __AVR_ATmega128__        //we're using atmega128A
 
 //define STATE number
-#define STATE_TIMER_INIT    0    
-#define STATE_TIMER_RUNNING    1
-#define STATE_TIMER_PAUSE      2
+#define STATE_INIT 0
+#define STATE_RUNNING 1
+#define STATE_PAUSE 2
 
 //include
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 //global variable declaration
-unsigned int state = STATE_TIMER_INIT;                                                      //initialize state
-unsigned timeNum = 0, timeIterruptSet = 0;                                                  //variable for time interrupt
+unsigned int state = STATE_INIT;                                                            //initialize state
+unsigned int timeNum = 0, timeInterruptExec = 0;                                            //variable for time interrupt
 unsigned char FND_SEGNP[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x6F}; //7segment decimal LED print with out point
 unsigned char FND_SEGWP[10] = {0xBF, 0x86, 0xDB, 0xCF, 0xE6, 0xED, 0xFD, 0xA7, 0xFF, 0xEF}; //7segment decimal LED print with point
 unsigned char FND_SEGPOS[4] = {0x01, 0x02, 0x04, 0x08};                                     //7segment's location
 unsigned int exp10[4] = {1, 10, 100, 1000};                                                 //print 10^x (for divide timeNum variable)
 ///////////////////////////////////////////////////////////
 
-///////////////////////////////segment print/////////////////////////////
-void printSeg(int num, int segmentLocation){
-    //by scemetic, PORTE must being clear before set segment's digit and led.
-    //because circuit use same port(A) to set segment's degit and led by latch.
-    
+
+void printSeg(int num, int segDigit){
+    //by schematic, PORTE must being clear before set segment's digit and led.
+    //because circuit use same port(A) to set segment's digit and led by latch.
+
     //set segment digit to print
-    PORTA = FND_SEGPOS[segmentLocation];
-    PORTE = 0x04;                            //set 1 segment digit latch
-    PORTE = 0x00;                            //clear port E
+    PORTA = FND_SEGPOS[segDigit];
+    PORTE = 0x04; //set 1 segment digit latch
+    PORTE = 0x00; //clear port E
 
     //print num at segment
-    PORTA = FND_SEGNP[num];
-    PORTE = 0x08;                //set 1 segment led latch
-    PORTE = 0x00;                //clear port E
-    
+    if(segDigit == 1) PORTA = FND_SEGWP[num];	//print with decimal point
+    else PORTA = FND_SEGNP[num];				//print with out decimal point
+    PORTE = 0x08; //set 1 segment led latch
+    PORTE = 0x00; //clear port E
 }
 
 void initPort(){
     //initialize port A and E
-    DDRA = 0xFF;                //set portA(7:0) to output
-    DDRE = 0x0C;                //set portE(3:2) to output
-    PORTE = 0x04;
+    DDRA = 0xFF; //set portA(7:0) to output
+    DDRE = 0x0C; //set portE(3:2) to output
+    PORTE = 0x04;//init port E
+    PORTA = 0x0F;//init port A
 }
-
 
 void initInterrupt(){
-    //timer interrupt 
+    //timer interrupt
     //normal mode(0), no prescaling
-    //when timer being overflow, interrupt exc
-    TCCR0 = 0x01;           //normal mode and no prescaling
-    TCNT0 = 0x00;
-    TIMSK = 0x01;           //overflow 활성화
-    TIFR = 0xff;            //초기화 임의 값을 넣으면 하드웨어적으로 초기화
+    //when timer being overflow, interrupt exec
+    TCCR0 = 0x01; //normal mode and no prescaling
+    TCNT0 = 0x00; //초기 count 값을 0으로 주어줌
+    TIMSK = 0x01; //overflow 활성화
+    TIFR = 0xff;  //초기화 - 임의 값을 넣으면 하드웨어적으로 초기화
+
     //external interrupt
-    
-    //external interrupt int4 enable 
-    //int4 버튼 falling edge활성화
-    EICRB  &= ~(1<<ISC40);
-    EICRB  |= 1<<ISC41;     //same to EICRB = 0d00000011
+    //external interrupt int4 enable
+    //active int4 when falling edge
+    EICRB &= ~(1 << ISC40);
+    EICRB |= 1 << ISC41; //same to EICRB = 0dxxxxxx10
     //int4 입력 활성화
-    EIMSK |= 1<<INT4;
+    EIMSK |= 1 << INT4;
 
     //external interrupt int5 enable
-    //int5 버튼 falling edge활성화
-    EICRB  &= ~(1<<ISC50);  //0으로 설정
-    EICRB  |= 1<<ISC51;     //1로 설정
+    //active int5 when falling edge
+    //same to EICRB = 0dxxxx10xx
+    EICRB &= ~(1 << ISC50); //0으로 설정
+    EICRB |= 1 << ISC51;    //1로 설정
     //int5 입력 활성화
-    EIMSK |= 1<<INT5;
-    
+    EIMSK |= 1 << INT5;
 }
 
-////////////////////////interrupt///////////////////
+////////////////////////interrupt service routine///////////////////
 ISR(INT4_vect){
-    //external interrupt launch when press int4 BT(falling edge)
-    //when state is init or pause, then change state to running
-    //when state is runnung, then change state to pause(keep timeNum)
-    if(state == STATE_TIMER_INIT || state == STATE_TIMER_PAUSE){
-        state = STATE_TIMER_RUNNING;
-    }else if(state == STATE_TIMER_RUNNING){
-        state = STATE_TIMER_PAUSE;
+    //external interrupt execution when press int4 BT(falling edge)
+    //when state is init or pause, change state to running
+    //when state is runnung, change state to pause(keep timeNum)
+    if(state == STATE_RUNNING){
+        state = STATE_PAUSE;    //pause stopwatch. keep print timeNum.
+    }else{
+        state = STATE_RUNNING;  //start(or resume) stopwatch
     }
+    
 }
 
 ISR(INT5_vect){
-    //external interrupt launch when press int4 BT(falling edge)
-    //when state is pause, then reset timeNum(print 0000) and change state to init
-    //when state is running, then reset timeNum(print 0000) and keep running
-    if(state == STATE_TIMER_PAUSE){
-        timeNum = 0;
-        state = STATE_TIMER_INIT;
-    }else if(state == STATE_TIMER_RUNNING){
-        timeNum = 0;
+    //external interrupt execution when press int5 BT(falling edge)
+    //when state is pause, reset timeNum(print 0000) and change state to init
+    //when state is running, reset timeNum(print 0000) and keep running
+    if(state == STATE_RUNNING){
+        timeNum = 0;            //clear timeNum and print "0000" but keep running stop watch
+    }else{
+        timeNum = 0;            //clear timeNum and print "0000"
+        state = STATE_INIT;     //initialize stopwatch
     }
 }
 
 ISR(TIMER0_OVF_vect){
-    //with out prescaling, time interrupt launch (16 MHz / 256 = 62500) / sec
-    //so, 10ms counted when interrupt's 625th launch 
-    timeIterruptSet++;
-    if((timeIterruptSet > 625) && (state == STATE_TIMER_RUNNING)){
-        timeNum++;
-        timeIterruptSet = 0;
+    //with out prescaling, time interrupt execution 62500 times / sec (16 MHz / 256 = 62500)
+    //so, we can count 10ms by count 625times interrupt execution 
+    //count interrupt execution in timeInterruptExec and increase timeNum when timeInterruptExec became 625
+    timeInterruptExec++;
+    if((timeInterruptExec > 625) && (state == STATE_RUNNING)){
+        timeNum++;              //count 10ms
+        timeInterruptExec = 0;  //clear execution count
     }
 }
-///////////////////inturrupt end////////////////////////
+///////////////////interrupt service routine end////////////////////////
 
-int main(void)
-{
-    //initialaze port and interrupt
+int main(void){
+    //initialize port and interrupt
     initPort();
     initInterrupt();
 
     //Global Interrupt Enable
     sei();      
     
-    //main function
     //print 7segment by global variable timeNum
-    //timeNum variable is increase by time intrrupt
+    //timeNum variable is increase by time interrupt
     while (1){
         //segment print
         for(int i = 0; i < 4; i++){
@@ -133,5 +128,4 @@ int main(void)
         //출력할 수가 9999를 넘으면 초기화
         if(timeNum == 10000) timeNum = 0;
     }
-    
 }
